9830d8a3f75e2117014f9fee573196ad5f187c94
Noah Moller <noah@tetrix.tech>
2025-01-17 10:48:49 +1000

    Fixed Xcode project formatting (again)
    Fixed Xcode project formatting (again)

--- Sources/crossui-cli/Generators.swift
+++ Sources/crossui-cli/Generators.swift
@@ -69,340 +69,44 @@ func generateMacOSProject(appName: String, rootView: any View) throws {
     """
     try infoPlistContent.write(toFile: infoPlistPath, atomically: true, encoding: .utf8)
     
-    // Generate project.pbxproj
-    let xcodeProjDir = macOSDir + "/\(appName).xcodeproj"
-    try FileManager.default.createDirectory(atPath: xcodeProjDir, withIntermediateDirectories: true, attributes: nil)
+    // Create Xcode project using xcodebuild
+    let process = Process()
+    process.executableURL = URL(fileURLWithPath: "/usr/bin/xcodebuild")
+    process.arguments = [
+        "-create",
+        "-template", "macosx.app",
+        "-destination", "\(macOSDir)/\(appName).xcodeproj",
+        "DEVELOPMENT_TEAM=", // Leave empty for now
+        "PRODUCT_NAME=\(appName)",
+        "PRODUCT_BUNDLE_IDENTIFIER=com.crossui.\(appName.lowercased())",
+        "MACOSX_DEPLOYMENT_TARGET=10.15",
+        "SWIFT_VERSION=5.0"
+    ]
     
-    let pbxprojPath = xcodeProjDir + "/project.pbxproj"
-    let pbxprojContent = """
-    // !$*UTF8*$!
-    {
-        archiveVersion = 1;
-        classes = {
-        };
-        objectVersion = 56;
-        objects = {
-            /* Begin PBXBuildFile section */
-            13B07FBC1A68108700A75B9A /* ContentView.swift in Sources */ = {isa = PBXBuildFile; fileRef = 13B07FB71A68108700A75B9A /* ContentView.swift */; };
-            13B07FBD1A68108700A75B9A /* \(appName)App.swift in Sources */ = {isa = PBXBuildFile; fileRef = 13B07FB61A68108700A75B9A /* \(appName)App.swift */; };
-            /* End PBXBuildFile section */
-            
-            /* Begin PBXFileReference section */
-            13B07F961A680F5B00A75B9A /* \(appName).app */ = {isa = PBXFileReference; explicitFileType = wrapper.application; includeInIndex = 0; path = \(appName).app; sourceTree = BUILT_PRODUCTS_DIR; };
-            13B07FB61A68108700A75B9A /* \(appName)App.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; name = "\(appName)App.swift"; path = "\(appName)/\(appName)App.swift"; sourceTree = "<group>"; };
-            13B07FB71A68108700A75B9A /* ContentView.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; name = ContentView.swift; path = \(appName)/ContentView.swift; sourceTree = "<group>"; };
-            13B07FB51A68108700A75B9A /* Info.plist */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = text.plist.xml; name = Info.plist; path = \(appName)/Info.plist; sourceTree = "<group>"; };
-            /* End PBXFileReference section */
-            
-            /* Begin PBXFrameworksBuildPhase section */
-            13B07F8C1A680F5B00A75B9A /* Frameworks */ = {
-                isa = PBXFrameworksBuildPhase;
-                buildActionMask = 2147483647;
-                files = (
-                );
-                runOnlyForDeploymentPostprocessing = 0;
-            };
-            /* End PBXFrameworksBuildPhase section */
-            
-            /* Begin PBXGroup section */
-            83CBB9F61A601CBA00E9B192 = {
-                isa = PBXGroup;
-                children = (
-                    13B07FB61A68108700A75B9A /* \(appName)App.swift */,
-                    13B07FB71A68108700A75B9A /* ContentView.swift */,
-                    13B07FB51A68108700A75B9A /* Info.plist */,
-                    83CBBA001A601CBA00E9B192 /* Products */,
-                );
-                indentWidth = 2;
-                sourceTree = "<group>";
-                tabWidth = 2;
-                usesTabs = 0;
-            };
-            83CBBA001A601CBA00E9B192 /* Products */ = {
-                isa = PBXGroup;
-                children = (
-                    13B07F961A680F5B00A75B9A /* \(appName).app */,
-                );
-                name = Products;
-                sourceTree = "<group>";
-            };
-            /* End PBXGroup section */
-            
-            /* Begin PBXNativeTarget section */
-            13B07F861A680F5B00A75B9A /* \(appName) */ = {
-                isa = PBXNativeTarget;
-                buildConfigurationList = 13B07F931A680F5B00A75B9A /* Build configuration list for PBXNativeTarget "\(appName)" */;
-                buildPhases = (
-                    13B07F871A680F5B00A75B9A /* Sources */,
-                    13B07F8C1A680F5B00A75B9A /* Frameworks */,
-                );
-                buildRules = (
-                );
-                dependencies = (
-                );
-                name = \(appName);
-                productName = \(appName);
-                productReference = 13B07F961A680F5B00A75B9A /* \(appName).app */;
-                productType = "com.apple.product-type.application";
-            };
-            /* End PBXNativeTarget section */
-            
-            /* Begin PBXProject section */
-            83CBB9F71A601CBA00E9B192 /* Project object */ = {
-                isa = PBXProject;
-                attributes = {
-                    LastUpgradeCheck = 1430;
-                    ORGANIZATIONNAME = "CrossUI";
-                    TargetAttributes = {
-                        13B07F861A680F5B00A75B9A = {
-                            LastSwiftMigration = 1120;
-                        };
-                    };
-                };
-                buildConfigurationList = 83CBB9FA1A601CBA00E9B192 /* Build configuration list for PBXProject "\(appName)" */;
-                compatibilityVersion = "Xcode 14.0";
-                developmentRegion = en;
-                hasScannedForEncodings = 0;
-                knownRegions = (
-                    en,
-                    Base,
-                );
-                mainGroup = 83CBB9F61A601CBA00E9B192;
-                productRefGroup = 83CBBA001A601CBA00E9B192 /* Products */;
-                projectDirPath = "";
-                projectRoot = "";
-                targets = (
-                    13B07F861A680F5B00A75B9A /* \(appName) */,
-                );
-            };
-            /* End PBXProject section */
-            
-            /* Begin PBXSourcesBuildPhase section */
-            13B07F871A680F5B00A75B9A /* Sources */ = {
-                isa = PBXSourcesBuildPhase;
-                buildActionMask = 2147483647;
-                files = (
-                    13B07FBC1A68108700A75B9A /* ContentView.swift in Sources */,
-                    13B07FBD1A68108700A75B9A /* \(appName)App.swift in Sources */,
-                );
-                runOnlyForDeploymentPostprocessing = 0;
-            };
-            /* End PBXSourcesBuildPhase section */
-            
-            /* Begin XCBuildConfiguration section */
-            13B07F941A680F5B00A75B9A /* Debug */ = {
-                isa = XCBuildConfiguration;
-                buildSettings = {
-                    ASSETCATALOG_COMPILER_APPICON_NAME = AppIcon;
-                    CLANG_ENABLE_MODULES = YES;
-                    CODE_SIGN_STYLE = Automatic;
-                    CURRENT_PROJECT_VERSION = 1;
-                    ENABLE_PREVIEWS = YES;
-                    INFOPLIST_FILE = \(appName)/Info.plist;
-                    LD_RUNPATH_SEARCH_PATHS = (
-                        "$(inherited)",
-                        "@executable_path/../Frameworks",
-                    );
-                    MACOSX_DEPLOYMENT_TARGET = 10.15;
-                    OTHER_LDFLAGS = (
-                        "$(inherited)",
-                    );
-                    PRODUCT_BUNDLE_IDENTIFIER = "com.crossui.\(appName.lowercased())";
-                    PRODUCT_NAME = \(appName);
-                    SWIFT_OPTIMIZATION_LEVEL = "-Onone";
-                    SWIFT_VERSION = 5.0;
-                };
-                name = Debug;
-            };
-            13B07F951A680F5B00A75B9A /* Release */ = {
-                isa = XCBuildConfiguration;
-                buildSettings = {
-                    ASSETCATALOG_COMPILER_APPICON_NAME = AppIcon;
-                    CLANG_ENABLE_MODULES = YES;
-                    CODE_SIGN_STYLE = Automatic;
-                    CURRENT_PROJECT_VERSION = 1;
-                    ENABLE_PREVIEWS = YES;
-                    INFOPLIST_FILE = \(appName)/Info.plist;
-                    LD_RUNPATH_SEARCH_PATHS = (
-                        "$(inherited)",
-                        "@executable_path/../Frameworks",
-                    );
-                    MACOSX_DEPLOYMENT_TARGET = 10.15;
-                    OTHER_LDFLAGS = (
-                        "$(inherited)",
-                    );
-                    PRODUCT_BUNDLE_IDENTIFIER = "com.crossui.\(appName.lowercased())";
-                    PRODUCT_NAME = \(appName);
-                    SWIFT_VERSION = 5.0;
-                };
-                name = Release;
-            };
-            83CBBA201A601CBA00E9B192 /* Debug */ = {
-                isa = XCBuildConfiguration;
-                buildSettings = {
-                    ALWAYS_SEARCH_USER_PATHS = NO;
-                    CLANG_ANALYZER_LOCALIZABILITY_NONLOCALIZED = YES;
-                    CLANG_CXX_LANGUAGE_STANDARD = "c++17";
-                    CLANG_CXX_LIBRARY = "libc++";
-                    CLANG_ENABLE_MODULES = YES;
-                    CLANG_ENABLE_OBJC_ARC = YES;
-                    CLANG_WARN_BLOCK_CAPTURE_AUTORELEASING = YES;
-                    CLANG_WARN_BOOL_CONVERSION = YES;
-                    CLANG_WARN_COMMA = YES;
-                    CLANG_WARN_CONSTANT_CONVERSION = YES;
-                    CLANG_WARN_DEPRECATED_OBJC_IMPLEMENTATIONS = YES;
-                    CLANG_WARN_DIRECT_OBJC_ISA_USAGE = YES_ERROR;
-                    CLANG_WARN_EMPTY_BODY = YES;
-                    CLANG_WARN_ENUM_CONVERSION = YES;
-                    CLANG_WARN_INFINITE_RECURSION = YES;
-                    CLANG_WARN_INT_CONVERSION = YES;
-                    CLANG_WARN_NON_LITERAL_NULL_CONVERSION = YES;
-                    CLANG_WARN_OBJC_IMPLICIT_RETAIN_SELF = YES;
-                    CLANG_WARN_OBJC_LITERAL_CONVERSION = YES;
-                    CLANG_WARN_OBJC_ROOT_CLASS = YES_ERROR;
-                    CLANG_WARN_QUOTED_INCLUDE_IN_FRAMEWORK_HEADER = YES;
-                    CLANG_WARN_RANGE_LOOP_ANALYSIS = YES;
-                    CLANG_WARN_STRICT_PROTOTYPES = YES;
-                    CLANG_WARN_SUSPICIOUS_MOVE = YES;
-                    CLANG_WARN_UNREACHABLE_CODE = YES;
-                    CLANG_WARN__DUPLICATE_METHOD_MATCH = YES;
-                    "CODE_SIGN_IDENTITY[sdk=iphoneos*]" = "iPhone Developer";
-                    COPY_PHASE_STRIP = NO;
-                    ENABLE_STRICT_OBJC_MSGSEND = YES;
-                    ENABLE_TESTABILITY = YES;
-                    "EXCLUDED_ARCHS[sdk=iphonesimulator*]" = i386;
-                    GCC_C_LANGUAGE_STANDARD = gnu99;
-                    GCC_DYNAMIC_NO_PIC = NO;
-                    GCC_NO_COMMON_BLOCKS = YES;
-                    GCC_OPTIMIZATION_LEVEL = 0;
-                    GCC_PREPROCESSOR_DEFINITIONS = (
-                        "DEBUG=1",
-                        "$(inherited)",
-                    );
-                    GCC_SYMBOLS_PRIVATE_EXTERN = NO;
-                    GCC_WARN_64_TO_32_BIT_CONVERSION = YES;
-                    GCC_WARN_ABOUT_RETURN_TYPE = YES_ERROR;
-                    GCC_WARN_UNDECLARED_SELECTOR = YES;
-                    GCC_WARN_UNINITIALIZED_AUTOS = YES_AGGRESSIVE;
-                    GCC_WARN_UNUSED_FUNCTION = YES;
-                    GCC_WARN_UNUSED_VARIABLE = YES;
-                    IPHONEOS_DEPLOYMENT_TARGET = 12.4;
-                    LD_RUNPATH_SEARCH_PATHS = (
-                        /usr/lib/swift,
-                        "@executable_path/Frameworks",
-                    );
-                    LIBRARY_SEARCH_PATHS = (
-                        "\"$(SDKROOT)/usr/lib/swift\"",
-                        "\"$(TOOLCHAIN_DIR)/usr/lib/swift/$(PLATFORM_NAME)\"",
-                        "\"$(inherited)\"",
-                    );
-                    MTL_ENABLE_DEBUG_INFO = YES;
-                    ONLY_ACTIVE_ARCH = YES;
-                    OTHER_CFLAGS = "$(inherited)";
-                    OTHER_CPLUSPLUSFLAGS = (
-                        "$(OTHER_CFLAGS)",
-                        "-DFOLLY_NO_CONFIG",
-                        "-DFOLLY_MOBILE=1",
-                        "-DFOLLY_USE_LIBCPP=1",
-                    );
-                    REACT_NATIVE_PATH = "${PODS_ROOT}/../../node_modules/react-native";
-                    SDKROOT = macosx;
-                };
-                name = Debug;
-            };
-            83CBBA211A601CBA00E9B192 /* Release */ = {
-                isa = XCBuildConfiguration;
-                buildSettings = {
-                    ALWAYS_SEARCH_USER_PATHS = NO;
-                    CLANG_ANALYZER_LOCALIZABILITY_NONLOCALIZED = YES;
-                    CLANG_CXX_LANGUAGE_STANDARD = "c++17";
-                    CLANG_CXX_LIBRARY = "libc++";
-                    CLANG_ENABLE_MODULES = YES;
-                    CLANG_ENABLE_OBJC_ARC = YES;
-                    CLANG_WARN_BLOCK_CAPTURE_AUTORELEASING = YES;
-                    CLANG_WARN_BOOL_CONVERSION = YES;
-                    CLANG_WARN_COMMA = YES;
-                    CLANG_WARN_CONSTANT_CONVERSION = YES;
-                    CLANG_WARN_DEPRECATED_OBJC_IMPLEMENTATIONS = YES;
-                    CLANG_WARN_DIRECT_OBJC_ISA_USAGE = YES_ERROR;
-                    CLANG_WARN_EMPTY_BODY = YES;
-                    CLANG_WARN_ENUM_CONVERSION = YES;
-                    CLANG_WARN_INFINITE_RECURSION = YES;
-                    CLANG_WARN_INT_CONVERSION = YES;
-                    CLANG_WARN_NON_LITERAL_NULL_CONVERSION = YES;
-                    CLANG_WARN_OBJC_IMPLICIT_RETAIN_SELF = YES;
-                    CLANG_WARN_OBJC_LITERAL_CONVERSION = YES;
-                    CLANG_WARN_OBJC_ROOT_CLASS = YES_ERROR;
-                    CLANG_WARN_QUOTED_INCLUDE_IN_FRAMEWORK_HEADER = YES;
-                    CLANG_WARN_RANGE_LOOP_ANALYSIS = YES;
-                    CLANG_WARN_STRICT_PROTOTYPES = YES;
-                    CLANG_WARN_SUSPICIOUS_MOVE = YES;
-                    CLANG_WARN_UNREACHABLE_CODE = YES;
-                    CLANG_WARN__DUPLICATE_METHOD_MATCH = YES;
-                    "CODE_SIGN_IDENTITY[sdk=iphoneos*]" = "iPhone Developer";
-                    COPY_PHASE_STRIP = YES;
-                    ENABLE_NS_ASSERTIONS = NO;
-                    ENABLE_STRICT_OBJC_MSGSEND = YES;
-                    "EXCLUDED_ARCHS[sdk=iphonesimulator*]" = i386;
-                    GCC_C_LANGUAGE_STANDARD = gnu99;
-                    GCC_NO_COMMON_BLOCKS = YES;
-                    GCC_WARN_64_TO_32_BIT_CONVERSION = YES;
-                    GCC_WARN_ABOUT_RETURN_TYPE = YES_ERROR;
-                    GCC_WARN_UNDECLARED_SELECTOR = YES;
-                    GCC_WARN_UNINITIALIZED_AUTOS = YES_AGGRESSIVE;
-                    GCC_WARN_UNUSED_FUNCTION = YES;
-                    GCC_WARN_UNUSED_VARIABLE = YES;
-                    IPHONEOS_DEPLOYMENT_TARGET = 12.4;
-                    LD_RUNPATH_SEARCH_PATHS = (
-                        /usr/lib/swift,
-                        "@executable_path/Frameworks",
-                    );
-                    LIBRARY_SEARCH_PATHS = (
-                        "\"$(SDKROOT)/usr/lib/swift\"",
-                        "\"$(TOOLCHAIN_DIR)/usr/lib/swift/$(PLATFORM_NAME)\"",
-                        "\"$(inherited)\"",
-                    );
-                    MTL_ENABLE_DEBUG_INFO = NO;
-                    OTHER_CFLAGS = "$(inherited)";
-                    OTHER_CPLUSPLUSFLAGS = (
-                        "$(OTHER_CFLAGS)",
-                        "-DFOLLY_NO_CONFIG",
-                        "-DFOLLY_MOBILE=1",
-                        "-DFOLLY_USE_LIBCPP=1",
-                    );
-                    REACT_NATIVE_PATH = "${PODS_ROOT}/../../node_modules/react-native";
-                    SDKROOT = macosx;
-                    VALIDATE_PRODUCT = YES;
-                };
-                name = Release;
-            };
-            /* End XCBuildConfiguration section */
-            
-            /* Begin XCConfigurationList section */
-            13B07F931A680F5B00A75B9A /* Build configuration list for PBXNativeTarget "\(appName)" */ = {
-                isa = XCConfigurationList;
-                buildConfigurations = (
-                    13B07F941A680F5B00A75B9A /* Debug */,
-                    13B07F951A680F5B00A75B9A /* Release */,
-                );
-                defaultConfigurationIsVisible = 0;
-                defaultConfigurationName = Release;
-            };
-            83CBB9FA1A601CBA00E9B192 /* Build configuration list for PBXProject "\(appName)" */ = {
-                isa = XCConfigurationList;
-                buildConfigurations = (
-                    83CBBA201A601CBA00E9B192 /* Debug */,
-                    83CBBA211A601CBA00E9B192 /* Release */,
-                );
-                defaultConfigurationIsVisible = 0;
-                defaultConfigurationName = Release;
-            };
-            /* End XCConfigurationList section */
-        };
-        rootObject = 83CBB9F71A601CBA00E9B192 /* Project object */;
+    try process.run()
+    process.waitUntilExit()
+    
+    if process.terminationStatus != 0 {
+        throw NSError(domain: "BuildError", code: 1, userInfo: [NSLocalizedDescriptionKey: "Failed to create Xcode project"])
     }
-    """
-    try pbxprojContent.write(toFile: pbxprojPath, atomically: true, encoding: .utf8)
+    
+    // Add source files to the project
+    let addFilesProcess = Process()
+    addFilesProcess.executableURL = URL(fileURLWithPath: "/usr/bin/xcodebuild")
+    addFilesProcess.arguments = [
+        "-project", "\(macOSDir)/\(appName).xcodeproj",
+        "-target", appName,
+        "-add", "\(macOSDir)/\(appName)/ContentView.swift",
+        "-add", "\(macOSDir)/\(appName)/\(appName)App.swift",
+        "-add", "\(macOSDir)/\(appName)/Info.plist"
+    ]
+    
+    try addFilesProcess.run()
+    addFilesProcess.waitUntilExit()
+    
+    if addFilesProcess.terminationStatus != 0 {
+        throw NSError(domain: "BuildError", code: 1, userInfo: [NSLocalizedDescriptionKey: "Failed to add source files to Xcode project"])
+    }
 }
 
 // Helper function to generate Xcode project content
